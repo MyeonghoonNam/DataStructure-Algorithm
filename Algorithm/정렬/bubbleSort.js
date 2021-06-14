1a2e8c8d6c09c500b91ffc78ef0019486d53d4aa
MyeongHoonNam <codenmh0822@naver.com>
2021-06-14 15:54:58 +0900

    버블 정렬 기능 개선

--- Algorithm/정렬/bubbleSort.js
+++ Algorithm/정렬/bubbleSort.js
@@ -1,9 +1,9 @@
 'use strict';
 
 function BubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length - 1; i++) {
     let swap = false;
-    for (let j = 0; j < arr.length - 1 - i; j++) {
+    for (let j = 0; j < arr.length - i; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         swap = true;
