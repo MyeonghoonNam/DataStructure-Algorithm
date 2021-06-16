688d0ecc86c09bacda0d4d511c68538501f95f81
MyeongHoonNam <codenmh0822@naver.com>
2021-06-16 18:11:12 +0900

    이중 연결 리스트 - 기능 구현

---
+++ DataStructure/Linked List/doublyLinkedList.js
@@ -0,0 +1,225 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.data = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  // 리스트의 데이터 탐색 : O(n)
+  find(idx) {
+    let curNode = this.head;
+    let count = 0;
+
+    while (count !== idx) {
+      curNode = curNode.next;
+      count++;
+    }
+
+    if (!curNode) {
+      console.log('해당 위치에 데이터가 존재하지 않습니다.');
+      return;
+    }
+
+    return curNode;
+  }
+
+  // 리스트의 맨 끝에 데이터 삽입 : 탐색 O(1) + 삽입 O(1) => O(1)
+  append(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+
+    return;
+  }
+
+  // 리스트의 맨 끝 데이터 삭제 : 탐색 O(1) + 삭제 O(1) => O(1)
+  removeLast() {
+    const removeNode = this.tail;
+
+    if (this.isEmpty()) {
+      console.log('이미 데이터가 존재하지 않습니다.\n');
+
+      return;
+    }
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+    }
+
+    this.length--;
+
+    return removeNode;
+  }
+
+  // 리스트의 맨 앞에 데이터 삽입 : 탐색 O(1) + 삽입 O(1) => O(1)
+  prepend(value) {
+    const newNode = new Node(value);
+
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+
+    return;
+  }
+
+  // 리스트의 맨 앞 데이터 삭제 : 탐색 O(1) + 삭제 O(1) => O(1)
+  removeFirst() {
+    const removeNode = this.head;
+
+    if (this.isEmpty()) {
+      console.log('이미 데이터가 존재하지 않습니다.\n');
+
+      return;
+    }
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+    }
+
+    this.length--;
+
+    return removeNode;
+  }
+
+  // 맨 앞과 뒤를 제외한 임의의 위치에 데이터 삽입 : 탐색 O(n) + 삽입 O(1) => O(n)
+  insert(idx, value) {
+    if (idx >= this.length) {
+      // 리스트에 존재하지 않은 인덱스 이다.
+      // 그러므로 리스트의 맨 뒤에 삽입, append 시간복잡도 반영
+
+      console.log(
+        '리스트에 존재하지 않는 인덱스이므로 리스트의 마지막에 데이터를 삽입하겠습니다.'
+      );
+      return this.append(value);
+    } else if (idx === 0) {
+      // 리스트의 맨 앞에 삽입, prepend 시간복잡도 반영
+      return this.prepend(value);
+    }
+
+    const newNode = new Node(value);
+    const prevNode = this.find(idx - 1);
+
+    prevNode.next.prev = newNode;
+    newNode.next = prevNode.next;
+    prevNode.next = newNode;
+    newNode.prev = prevNode;
+
+    this.length++;
+
+    return;
+  }
+
+  // 맨 앞과 뒤를 제외한 임의의 위치에 데이터 삭제 : 탐색 O(n) + 삭제 O(1) => O(n)
+  remove(idx) {
+    if (idx >= this.length) {
+      // 리스트에 존재하지 않은 인덱스 이다.
+      console.log('리스트에 존재하지 않는 인덱스입니다.');
+
+      return;
+    } else if (idx === 0) {
+      // 리스트의 맨 앞 데이터 삭제이므로, removeFirst 시간복잡도 반영
+      return this.removeFirst();
+    } else if (idx === this.length - 1) {
+      // 리스트의 맨 뒤 데이터 삭제이므로, removeLast 시간복잡도 반영
+      return this.removeLast();
+    }
+
+    const removeNode = this.find(idx);
+    const prevNode = removeNode.prev;
+
+    prevNode.next = removeNode.next;
+    removeNode.next.prev = prevNode;
+  }
+
+  // 리스트의 데이터 포함 여부
+  isEmpty() {
+    return this.length === 0 ? true : false;
+  }
+
+  // 리스트의 요소들을 보기 쉽게 프린트
+  printList() {
+    let curNode = this.head;
+    let str = '';
+
+    while (curNode) {
+      str += curNode.data + ' ';
+      curNode = curNode.next;
+    }
+
+    if (str.length === 0) {
+      console.log('데이터가 존재하지 않습니다.\n');
+    } else {
+      console.log(`List Size : ${this.length} \n${str} \n`);
+    }
+
+    return;
+  }
+}
+
+const doublyLinkedList = new DoublyLinkedList();
+
+doublyLinkedList.append(1);
+doublyLinkedList.printList();
+
+doublyLinkedList.append(2);
+doublyLinkedList.printList();
+
+doublyLinkedList.removeLast();
+doublyLinkedList.printList();
+
+doublyLinkedList.removeLast();
+doublyLinkedList.printList();
+
+doublyLinkedList.prepend(1);
+doublyLinkedList.printList();
+
+doublyLinkedList.prepend(2);
+doublyLinkedList.printList();
+
+doublyLinkedList.removeFirst();
+doublyLinkedList.printList();
+
+doublyLinkedList.insert(1, 2);
+doublyLinkedList.printList();
+
+doublyLinkedList.insert(1, 3);
+doublyLinkedList.printList();
+
+doublyLinkedList.remove(1);
+doublyLinkedList.printList();
+
+console.log(doublyLinkedList);
