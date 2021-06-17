825b88ff1071feb8dfe493b0a0092cd9492d7b1e
MyeongHoonNam <codenmh0822@naver.com>
2021-06-18 03:36:40 +0900

    해시 테이블 - 기능 개선

--- DataStructure/HashTable/hashTable_v1.js
+++ DataStructure/HashTable/hashTable_v1.js
@@ -2,37 +2,77 @@
 
 // 개방주소법 : 이중 해싱을 적용한 해시 테이블 구현
 
-const myTableSize = 23; // 테이블 사이즈가 소수여야 효과가 좋다
-const myHashTable = [];
-
-const getSaveHash = (value) => value % myTableSize;
-
+// 테이블 사이즈가 소수여야 효과가 좋다
 // 스텝 해시에 사용되는 수는 테이블 사이즈보다 약간 작은 소수를 사용한다.
-const getStepHash = (value) => 17 - (value % 17);
 
-const setValue = (value) => {
-  let index = getSaveHash(value);
-  let targetValue = myHashTable[index];
-  while (true) {
-    if (!targetValue) {
-      myHashTable[index] = value;
-      console.log(`${index}번 인덱스에 ${value} 저장! `);
-      return;
-    } else if (myHashTable.length >= myTableSize) {
-      console.log('풀방입니다');
-      return;
-    } else {
-      console.log(`${index}번 인덱스에 ${value} 저장하려다 충돌 발생!ㅜㅜ`);
-      index += getStepHash(value);
-      index = index > myTableSize ? index - myTableSize : index;
-      targetValue = myHashTable[index];
+// 테이블사이즈와 두 번째 해시함수 둘 중에 하나가 소수가 아니라면 결국 언젠가 같은 해싱이 반복되기 때문이다.
+
+class HashTable {
+  constructor(size) {
+    this.table = new Array(size); // 해시테이블의 크기
+    this.size = 0; // 해시테이블에 채워진 데이터 크기
+  }
+
+  getFirstHash(key) {
+    return Number(key) % this.table.length;
+  }
+
+  getSecondHash(key) {
+    return this.table.length - 6 - (key % (this.table.length - 6));
+  }
+
+  setValue(data) {
+    let idx = this.getFirstHash(Object.keys(data)[0]);
+
+    while (true) {
+      if (!this.table[idx]) {
+        this.table[idx] = data;
+        console.log(`${idx}번 인덱스에 ${Object.values(data)[0]} 저장 !`);
+        this.size++;
+        return;
+      } else if (this.size >= this.table.length) {
+        console.log('해시테이블이 가득 찼습니다.');
+        return;
+      } else {
+        console.log(
+          `${idx}번 인덱스에 ${Object.values(data)[0]} 저장하려다 충돌 발생 !`
+        );
+        idx += this.getSecondHash(Object.keys(data));
+        idx = idx > this.table.size ? idx - this.table.length : idx;
+      }
     }
   }
-};
 
-setValue(1991);
-setValue(6);
-setValue(13);
-setValue(21);
+  getValue(key) {
+    let idx = this.getFirstHash(key);
 
-console.log(myHashTable);
+    if (!this.table[idx]) {
+      console.log('존재하지 않는 데이터입니다.');
+      return;
+    }
+
+    while (true) {
+      if (Object.keys(this.table[idx]).includes(String(key))) {
+        console.log(Object.values(this.table[idx])[0]);
+        return;
+      } else {
+        idx += this.getSecondHash(key);
+        idx = idx > this.table.size ? idx - this.table.length : idx;
+      }
+    }
+  }
+}
+
+const hashTable = new HashTable(23);
+
+hashTable.setValue({ 1991: 1 });
+hashTable.setValue({ 6: 2 });
+hashTable.setValue({ 13: 3 });
+hashTable.setValue({ 21: 4 });
+
+hashTable.getValue(1991);
+hashTable.getValue(6);
+hashTable.getValue(13);
+hashTable.getValue(21);
+
+console.log(hashTable);
