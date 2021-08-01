248fee34eb655c3708e2594057d2745e5f309617
MyeongHoonNam <codenmh0822@naver.com>
2021-08-01 20:16:50 +0900

    lowerBound 알고리즘 구현

---
+++ Algorithm/탐색/lowerBound.js
@@ -0,0 +1,29 @@
+const arr = [1, 2, 3, 3, 3, 4, 4, 6, 8, 9];
+
+// 중복된 값이 있더라도 그 값중에서 맨 앞의 위치를 반환한다.
+console.log(lowerBound(arr, 3));
+
+// 찾고자 하는 값이 존재하지 않더라도 5이상의 값의 위치를 반환한다.
+console.log(lowerBound(arr, 5));
+
+function lowerBound(arr, data) {
+  let result = Number.MAX_SAFE_INTEGER;
+
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] < data) {
+      start = mid + 1;
+
+      continue;
+    }
+
+    result = Math.min(result, mid);
+    end = mid - 1;
+  }
+
+  return result;
+}
