00de6466bd6e30230d39e6de3d43b4d8bccb987e
MyeongHoonNam <codenmh0822@naver.com>
2023-03-13 20:00:27 +0900

    최장 공통 문자열 자체를 구하는 알고리즘 추가

--- Algorithm/LCS/LongestCommonSubstring.js
+++ Algorithm/LCS/LongestCommonSubstring.js
@@ -5,6 +5,7 @@ const LCS = Array.from(new Array(A.length + 1), () =>
 );
 
 let maxLCSLength = 0;
+let maxLCSIndex = 0;
 
 for (let i = 1; i <= A.length; i++) {
   for (let j = 1; j <= B.length; j++) {
@@ -12,12 +13,13 @@ for (let i = 1; i <= A.length; i++) {
       LCS[i][j] = LCS[i - 1][j - 1] + 1;
     }
 
-    // 최종 공통 문자열 길이
     if (LCS[i][j] > maxLCSLength) {
       maxLCSLength = LCS[i][j];
+      maxLCSIndex = i;
     }
   }
 }
 
-console.log(LCS);
-console.log(maxLCSLength);
+console.log(LCS); // LCS 2차원 배열
+console.log(maxLCSLength); // 최장 공통 문자열 길이
+console.log(A.slice(maxLCSIndex - maxLCSLength, maxLCSIndex)); // 최장 공통 문자열
